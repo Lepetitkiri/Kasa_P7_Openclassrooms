0e7493758b51a0edc4a157ec06b438e69994218a
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-08-19 15:05:41 +0200

    GlobalStyle file added

---
+++ src/Utils/GlobalStyle.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import colors from './Colors';
+
+const GlobalStyle = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    font-family: Montserrat, 'Courier New', Courier, sans-serif;
+    color: ${colors.primary};
+    background-color: ${colors.white};
+  }
+
+  .container {
+    box-sizing: border-box;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${colors.primary};
+  }
+
+  ::placeholder {
+    color: ${colors.white};
+  }
+
+  html, body, div, ul, ol, li, p, h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './Utils/GlobalStyle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <div className="container">
+        <App />
+      </div>
+    </GlobalStyle>
   </React.StrictMode>
 );
