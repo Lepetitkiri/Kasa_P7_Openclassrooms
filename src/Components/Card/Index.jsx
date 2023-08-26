8c8e97434f06439ff27604fc3fb6182dd497b025
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-08-26 14:49:33 +0200

    map() fonction initialized in card component

--- src/Components/Card/Index.jsx
+++ src/Components/Card/Index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import GalleryCardStyle from "../Card/Style.jsx";
+import datas from '../../Ressources/housings.json';
+
+{/*Creation d'un Array contenant uniquement les titres des locations à partir des datas JSON*/ }
+let dataArrayTitles = datas.map(item => item.title);
+{/*Creation d'un Array contenant uniquement les images de couverture à partir des datas JSON*/ }
+let dataArrayCover = datas.map(item => item.cover);
 
 function GalleryCard() {
   return (
-    <GalleryCardStyle className="galleryCard__home">
-      <p>GalleryCard en cours</p>
-    </GalleryCardStyle>
+    <div>{
+      /*Premier mappage : à partir du tableau contenant les titres*/
+      dataArrayTitles.map((title, index) => (
+
+        /*Deuxieme mappage : à partir du tableau contenant les images de couverture*/
+        dataArrayCover.map((cover, ind) => (
+
+          < GalleryCardStyle key={`${index}${ind}`}> {title} + {cover} </GalleryCardStyle>
+        )
+        )
+      ))
+    } </div>
   );
 }
 
