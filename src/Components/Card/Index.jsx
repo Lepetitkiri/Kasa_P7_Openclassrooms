1aff76e3f4e0642fca0ba79046ab1a3c62e09fcb
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-08-27 15:56:00 +0200

    Map() fonction rebuild on Card component

--- src/Components/Card/Index.jsx
+++ src/Components/Card/Index.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import GalleryCardStyle from "../Card/Style.jsx";
 import datas from '../../Ressources/housings.json';
 
-
-{/*Creation d'un Array contenant uniquement les titres des locations à partir des datas JSON*/ }
-let dataArrayTitles = datas.map(item => item.title);
-
 function GalleryCard() {
+
+  {/*Récupération des infos attendues à partir des datas JSON des locations*/ }
+  const dataArrayForCards = datas.map(item => ({
+    id: item.id,
+    title: item.title,
+    image: item.cover
+  }));
+
   return (
     <>
-      {
-        /*Premier mappage : à partir du tableau contenant les titres*/
-        dataArrayTitles.map((title, index) => (
-
-          < GalleryCardStyle key={`${index}`}> {title} </GalleryCardStyle>
-        )
-        )
+      {dataArrayForCards.map(card => (
+        <GalleryCardStyle key={`card-${card.id}`} >
+          <p>{card.title}</p>
+          <img src={card.image} alt={card.title} />
+        </GalleryCardStyle >
+      ))
       }
     </>
   );
