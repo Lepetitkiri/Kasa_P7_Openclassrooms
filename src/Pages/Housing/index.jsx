b199e4574d586affb7033077f1875001af1abe6a
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-08-29 16:33:12 +0200

    Housing page routing bseing secured

--- src/Pages/Housing/index.jsx
+++ src/Pages/Housing/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
+import datas from '../../Ressources/housings.json';
 
 {/*Components*/ }
 import Header from '../../Components/Header/Index';
 import Footer from '../../Components/Footer/Index';
 
 function Housing() {
+
+    {/* Récupération de l'id associé au logement : */ }
+    const { housingId } = useParams();
+
+    {/*Recherche de correspondance entre la base de données et l'ID récupéré dans l'URL */ }
+    const isHousingIdCorrect = datas.find(item => item.id === housingId);
+
+    {/* Si isHousingCorrect renvoi "undefined", cela signifie que l'Id récupéré est mauvais */ }
+    isHousingIdCorrect === undefined ? console.log("Erreur") : console.log("Id conforme");
+
+
     return (
         <>
             <Header />
