b6587e77f682c273c80cce93dd904765040c9fbc
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-09-05 10:54:29 +0200

    Implementation of the carousel operating logic

--- src/Components/Slideshow/Index.jsx
+++ src/Components/Slideshow/Index.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import SlideshowStyled from './Style';
 
+const picture = [
+  "../Pictures/Image_de_test1.jpg",
+  "../Pictures/Image_de_test2.jpg"
+];
+
 function Slideshow() {
+
+  {/*Initialisation du numéro de l'image courante dans le useState */ }
+  const [currentPicture, setCurrentPicture] = useState(0);
+
+  {/*Fonction permettant de définir l'indice précédent */ }
+  function previousPicture() {
+    setCurrentPicture((prevIndex) => (prevIndex === 0 ? picture.length - 1 : prevIndex - 1));
+  }
+  {/*Fonction permettant de définir l'indice suivant */ }
+  function nextPicture() {
+    setCurrentPicture((prevIndex) => (prevIndex === picture.length - 1 ? 0 : prevIndex + 1));
+  }
+
   return (
     <SlideshowStyled>
-      <img src='../Pictures/Image_de_test1.jpg' alt="alt de test 1" className="slideshow__picture"></img>
-      <img src='../Pictures/Image_de_test2.jpg' alt="alt de test 2" className="slideshow__picture"></img>
-      <img src='../Pictures/FlecheVersLaGauche.png' alt="Fleche vers la gauche" className="slideshow__leftArrow"></img>
-      <p>1/2</p>
-      <img src='../Pictures/FlecheVersLaDroite.png' alt="Fleche vers la droite" className="slideshow__rightArrow"></img>
-    </SlideshowStyled>
+      <img src={picture[currentPicture]} alt="alt de test 1" className="slideshow__picture" />
+      <button className="slideshow__leftArrow" onClick={previousPicture}>
+        <img src='../Pictures/FlecheVersLaGauche.png' alt="Fleche vers la gauche" />
+      </button>
+      <p>{currentPicture + 1} / {picture.length} </p>
+      <button className="slideshow__rightArrow" onClick={nextPicture}>
+        <img src='../Pictures/FlecheVersLaDroite.png' alt="Fleche vers la droite" />
+      </button>
+    </SlideshowStyled >
   );
 }
 
