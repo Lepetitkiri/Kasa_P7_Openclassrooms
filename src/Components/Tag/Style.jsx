4a0d36671bdca9e1841836e14aa547e498723496
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-09-11 10:46:24 +0200

    Tag component style ended

--- src/Components/Tag/Style.jsx
+++ src/Components/Tag/Style.jsx
@@ -4,6 +4,25 @@ import colors from '../../Utils/Colors';
 const TagStyle = styled.div`
 
   display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 115px;
+  height: 25px;
+  background-color: ${colors.primary};
+  color: ${colors.white};
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+
+/* Version mobile */
+@media only screen and (max-width: 767px) {
+
+  width: 84px;
+  height: 18px;
+  font-size: 10px;
+  line-height: 14px;
+  }
 
 `;
 
